9a68aff6ec1089b561dc147d557a40e6803b9240
Joseph Adams <joseph.adams@blueyonder.co.uk>
2023-09-12 20:43:38 +0100

    Added placeholder components

---
+++ src/components/Dashboard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Dashboard(props) {
+  return <div>Here I am</div>;
+}
+
+export default Dashboard;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      &copy; some words in here to make it look better
+    </div>
+  );
+}
+
+export default Footer;
