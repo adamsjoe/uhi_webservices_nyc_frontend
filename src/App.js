8121f472e624dbc5f905869241a00fc059ebaee1
Joseph Adams <joseph.adams@blueyonder.co.uk>
2023-09-14 11:59:11 +0100

    Added basic axios retrieve for boroughs Set up the <Select>  to show boroughs Added a handler for selecting (test only)

--- src/App.js
+++ src/App.js
@@ -1,34 +1,50 @@
 import React, { useState, useEffect } from "react";
 
 import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter, Link } from "react-router-dom";
+// import { BrowserRouter, Link } from "react-router-dom";
+import axios from "axios";
 
 import "./App.css";
 
+const baseAPIURL = "http://localhost:4000";
+
 export default function App() {
   const [boroughs, setBoroughs] = useState([]);
+  const [chosenBorough, setChosenBorough] = useState("None");
 
   useEffect(() => {
-    fetch("http://localhost:4000/historic/boroughs", {
-      method: "GET",
-      mode: "no-cors",
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setBoroughs(data);
+    axios
+      .get(baseAPIURL + "/historic/boroughs")
+      .then((response) => {
+        setBoroughs(response.data);
       })
-      .catch((err) => {
-        console.log(err.message);
+      .catch((error) => {
+        console.error(error);
       });
   }, []);
+
+  const handleSelectChange = (event) => {
+    setChosenBorough(event.target.value);
+  };
 
   return (
     <div className="container">
       New York City Accident Visualiser
       <div className="row">
-        <div className="col-md-4">Select a borough from the list below:</div>
-        <div className="col-md-8">DATA</div>
+        <div className="col-md-2">
+          Select a borough from the list below:
+          <select onChange={handleSelectChange} value={chosenBorough}>
+            <option key="NULL" value="NULL">
+              Pick from me :)
+            </option>
+            {boroughs.map((borough) => (
+              <option key={borough} value={borough}>
+                {borough}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="col-md-10">You picked the {chosenBorough} borough</div>
       </div>
     </div>
   );
