92e960c9cb26f237f53cc3918a6c0c96585c3d33
Joseph Adams <joseph.adams@blueyonder.co.uk>
2023-09-17 16:46:18 +0100

    Made a nicer datablock

--- src/components/Datablock.js
+++ src/components/Datablock.js
@@ -1,30 +1,81 @@
 import React from "react";
 import { translateDay } from "../helpers/translateDay";
+import { translateMonth } from "../helpers/translateMonth";
 
 function Datablock({ data }) {
-  console.log(data);
-  const temp = data.map((item) => item.TEMP);
-  const visibilty = data.map((item) => item.VISIB);
-  const windSpeec = data.map((item) => item.WDSP);
-  const maxTemp = data.map((item) => item.MAX);
-  const minTemp = data.map((item) => item.MIN);
-  const fog = data.map((item) => item.FOG);
-  const rain = data.map((item) => item.PRCP);
-  const snow = data.map((item) => item.SNDP);
+  const totalCollisions = data.reduce(
+    (total, item) => total + item.NUM_COLS,
+    0
+  );
 
+  const getCyclistInjuries = data.reduce(
+    (total, item) => total + item.CYC_INJD,
+    0
+  );
+
+  const getCyclistDeath = data.reduce(
+    (total, item) => total + item.CYC_KILL,
+    0
+  );
+
+  const getMotoristInjuries = data.reduce(
+    (total, item) => total + item.MOTO_INJD,
+    0
+  );
+
+  const getMotoristDeath = data.reduce(
+    (total, item) => total + item.MOTO_KILL,
+    0
+  );
+
+  const getPedestriansInjuries = data.reduce(
+    (total, item) => total + item.PEDS_INJD,
+    0
+  );
+
+  const getPedestriansDeath = data.reduce(
+    (total, item) => total + item.PEDS_KILL,
+    0
+  );
+
+  const getPersonsInjuries = data.reduce(
+    (total, item) => total + item.PERS_INJD,
+    0
+  );
+
+  const getPersonsDeath = data.reduce(
+    (total, item) => total + item.PERS_KILL,
+    0
+  );
+
+  const totalFatalities =
+    getCyclistDeath + getMotoristDeath + getPedestriansDeath + getPersonsDeath;
+  const fatalityRate = (totalFatalities / totalCollisions) * 100;
   return (
     <>
-      {data[0].DAY}/{data[0].MONTH}/{data[0].YEAR} was a{" "}
-      {translateDay(data[0].WEEKDAY)} <br />
-      Average temperature was {temp}F <br />
-      The Max temperature was {maxTemp}F <br />
-      The Min temperature was {minTemp}F <br />
-      There was {rain} inches of rain recorded <br />
-      {fog[0] === 1 ? "It was foggy" : "No fog"} <br />
-      {snow[0] !== 999.9
-        ? `The snow was ${snow} inches`
-        : "No snow this day"}{" "}
-      <br />
+      During {translateMonth(data[0].MONTH)}, {data[0].YEAR} there were: <br />
+      <ul>
+        <li>
+          A total of <b>{totalCollisions}</b> Collisions.
+        </li>
+        <li>
+          A total of {getCyclistInjuries} cyclists injured and {getCyclistDeath}{" "}
+          cyclists killed.
+        </li>
+        <li>
+          A total of {getMotoristInjuries} motorists injured and{" "}
+          {getMotoristDeath} motorists killed.
+        </li>
+        <li>
+          A total of {getPedestriansInjuries} pedestrians injured and{" "}
+          {getPedestriansDeath} pedestrians killed.
+        </li>
+        <li>
+          A total of {getPersonsInjuries} persons (individuals) injured and{" "}
+          {getPersonsDeath} persons (individuals) killed.
+        </li>
+        <li>The fatality rate for this month is {fatalityRate.toFixed(2)}%</li>
+      </ul>
     </>
   );
 }
