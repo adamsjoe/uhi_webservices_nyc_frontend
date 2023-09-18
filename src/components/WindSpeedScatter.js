56bbc020d526b70543e2c62dc9318ecd349b55c0
Joseph Adams <joseph.adams@blueyonder.co.uk>
2023-09-18 21:20:50 +0100

    Fixed scatter with correctly formatted data

--- src/components/WindSpeedScatter.js
+++ src/components/WindSpeedScatter.js
@@ -2,34 +2,56 @@ import React from "react";
 import { Scatter } from "react-chartjs-2";
 
 const WindSpeedScatter = ({ data }) => {
-  const labels = data.map((item) => `${item.MONTH}-${item.YEAR}`);
-  const temp = data.map((item) => item.TEMP);
-  const windspeed = data.map((item) => item.WDSP);
+  const scatterData = data.map((item) => ({
+    x: item.TEMP,
+    y: item.WDSP,
+    label: `${item.DAY}-${item.YEAR}`,
+  }));
 
   const chartData = {
-    labels: labels,
     datasets: [
       {
-        label: "Temperature",
-        data: temp,
-        yAxisID: "y",
-        fill: false,
-        backgroundColor: "rgba(0, 255, 0, 0.2)",
-        borderColor: "rgba(0, 255, 0, 1)",
-        borderWidth: 1,
-      },
-      {
-        label: "Windspeed",
-        data: windspeed,
-        yAxisID: "y",
-        fill: false,
-        backgroundColor: "rgba(255, 165, 0, 0.2)",
-        borderColor: "rgba(255, 165, 0, 1)",
-        borderWidth: 1,
+        label: "Temperature vs Wind Speed",
+        data: scatterData,
+        showLine: false,
+        pointRadius: 5,
+        pointBackgroundColor: "rgba(0, 255, 0, 0.7)",
+        pointBorderColor: "rgba(0, 255, 0, 1)",
       },
     ],
   };
-  return <Scatter data={chartData} />;
+
+  const chartOptions = {
+    scales: {
+      x: {
+        type: "linear",
+        position: "bottom",
+        title: {
+          display: true,
+          text: "Temperature (°F)",
+        },
+      },
+      y: {
+        type: "linear",
+        title: {
+          display: true,
+          text: "Wind Speed (mph)",
+        },
+      },
+    },
+    plugins: {
+      tooltip: {
+        callbacks: {
+          label: (context) => {
+            const label = context.dataset.data[context.dataIndex].label;
+            return label;
+          },
+        },
+      },
+    },
+  };
+
+  return <Scatter data={chartData} options={chartOptions} />;
 };
 
 export default WindSpeedScatter;
